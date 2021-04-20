2dd12f76cc2d808f01d8200fe027aa263364e5a3
Francesco Stucci <francesco.stucci@horsa.it>
2021-04-20 11:09:02 +0200

    link fix

--- src/app/pages/order/order.module.ts
+++ src/app/pages/order/order.module.ts
@@ -4,6 +4,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 import { NgImageSliderModule } from 'ng-image-slider';
 import { OrderComponent } from './order.component';
 
@@ -11,6 +12,7 @@ import { OrderComponent } from './order.component';
     imports: [
         CommonModule,
         FormsModule,
+        RouterModule,
         FlexLayoutModule,
         MatIconModule,
         NgImageSliderModule,
