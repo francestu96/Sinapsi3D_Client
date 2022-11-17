4bd5a81b78001038e085a7b8f6cd1dd3cc9ce9d8
francestu96 <francestu96@gmail.com>
2022-11-17 14:57:44 +0100

    baseURL fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  baseURL: 'sinapsi3d-server.vercel.app/api'
+  baseURL: 'https://sinapsi3d-server.vercel.app/api'
   // baseURL: 'http://localhost:1080/api'
 };
